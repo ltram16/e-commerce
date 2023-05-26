b618eff6d0c32373d8378a4e9e34357d4b31b2a7
ltram16 <ltram16@gmail.com>
2023-05-25 20:03:02 -0700

    edited index file in models folder

--- models/index.js
+++ models/index.js
@@ -11,10 +11,35 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: Product,
+    unique: false
+  },
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.hasMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
+
 
 module.exports = {
   Product,
